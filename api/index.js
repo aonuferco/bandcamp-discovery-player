fc9f6498c14b42ed52becda7e5fce6400bd41f2a
your-correct-username <cool-guli@mail.ru>
2025-08-01 16:17:48 -0400

    vercel deployment changes;

--- api/index.js
+++ api/index.js
@@ -1,4 +1,20 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
 import { app } from "../src/server/app.js";
 
-// Export the Express app for Vercel serverless functions
-export default app;
+// Get __dirname equivalent for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Create a new app instance for Vercel
+const vercelApp = express();
+
+// Serve static files from the public directory (Vercel will build this)
+vercelApp.use(express.static(path.join(__dirname, "../public")));
+
+// Use the main app for all routes
+vercelApp.use(app);
+
+// Export the Vercel app
+export default vercelApp;
